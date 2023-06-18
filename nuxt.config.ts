565ca0affade5b3ec65d88ffc0221853f4178797
TzuWei <ap9940506@gmail.com>
2023-06-18 14:56:20 +0800

    feat: initialize the project with nuxi, eslint, and prettier

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    shim: false,
+  },
+  modules: ["@nuxtjs/eslint-module"],
+});
